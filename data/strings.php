77af655b4a322e401e5e075ffb87ef251ac324ca
dominhhieu1405 <51281387+dominhhieu1405@users.noreply.github.com>
2020-04-05 17:28:10 +0700

    Create strings.php

---
+++ data/strings.php
@@ -0,0 +1,11 @@
+<?
+function strings( $length ) {
+$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+$size = strlen( $chars );
+for( $i = 0; $i < $length; $i++ ) {
+$str .= $chars[ rand( 0, $size - 1 ) ];
+ }
+return $str;
+}
+$string = strings( 5 );
+?>
