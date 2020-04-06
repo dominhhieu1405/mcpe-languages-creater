38dec5f8aaa8449cd14c37ebe4d278336e331197
dominhhieu1405 <51281387+dominhhieu1405@users.noreply.github.com>
2020-04-06 20:34:51 +0700

    Update zip.php

--- data/zip.php
+++ data/zip.php
@@ -1,2 +1,11 @@
 <?php
 $file = new ZipArchive();
+$file->open("file/" . $string . ".zip", ZipArchive::CREATE);
+$file->addEmptyDir("texts/");
+$file->addFile('/lang/' . $_POST['lang'] . '.lang', '/texts/' . $_POST['lang'] . '.lang');
+$file->addFile($icon, 'pack_icon.png');
+$file->addFromString( "/texts/languages.json" , "[ "$_POST['lang']" ]" );
+$file->addFromString( "/texts/language_names.json" , "[ [ "$_POST['lang']", "$langname" ] ]" );
+$file->addFromString( "manifest.json" , "");
+$file->close();
+?> 
