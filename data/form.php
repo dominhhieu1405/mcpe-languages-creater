2b326d6d491a60d0e141a33981beac073da0d4cb
dominhhieu1405 <51281387+dominhhieu1405@users.noreply.github.com>
2020-04-09 16:20:32 +0700

    Update form.php

--- data/form.php
+++ data/form.php
@@ -1,13 +1,13 @@
 <form action="/upload.php" method="post" enctype="multipart/form-data" autocomplete="on">
     Chọn Icon Addon:
-    <input type="file" name="icon" id="icon">
+    <input type="file" name="icon" id="file">
     <br>
     Tên Addon:
-    <input type="text" name="name" id="name">
+    <input type="text" name="name" id="text">
     Mô tả:
     <textarea name="describe" id="describe"></textarea>
     Tác giả:
-    <input type="text" name="author" id="author">
+    <input type="text" name="author" id="text">
     Chọn ngôn ngữ:
     <select name="lang" id="lang">
     <?php
